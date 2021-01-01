6994f6c2416868cbdc4c81cdd7b98e8ddefeb54d
ChaAroiMak <72292809+ChaAroiMak@users.noreply.github.com>
2021-01-01 15:18:44 +0100

    Add files via upload

--- test/chess/UsageTests.java
+++ test/chess/UsageTests.java
@@ -4,6 +4,7 @@ import Board.ChessBoardPosition;
 import Exceptions.GameException;
 import Exceptions.StatusException;
 import Pieces.ChessPieceColor;
+import Pieces.ChessPieceNumber;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -28,16 +29,15 @@ public class UsageTests {
 
 
     @Test
-    public void goodPickColor1() throws GameException, StatusException {
+    public void goodPickColor1() throws GameException, StatusException { //player chooses color
         ChessGame chess = this.getColor();
 
         ChessPieceColor spieler1Color = chess.pick(SPIELER1, ChessPieceColor.white);
         Assert.assertEquals(ChessPieceColor.white, spieler1Color);
-
     }
 
     @Test
-    public void goodPickColor2() throws GameException, StatusException {
+    public void goodPickColor2() throws GameException, StatusException { //2players choose different colors
         ChessGame chess = this.getColor();
 
         ChessPieceColor spieler1Color = chess.pick(SPIELER1, ChessPieceColor.white);
@@ -47,7 +47,7 @@ public class UsageTests {
     }
 
     @Test
-    public void goodPickColor3() throws GameException, StatusException {
+    public void goodPickColor3() throws GameException, StatusException { //2 players choose same color
         ChessGame chess = this.getColor();
 
         ChessPieceColor spieler1Color = chess.pick(SPIELER1, ChessPieceColor.white);
@@ -57,7 +57,7 @@ public class UsageTests {
     }
 
     @Test
-    public void goodPickColor4() throws GameException, StatusException {
+    public void goodPickColor4() throws GameException, StatusException { //2 players choose same color
         ChessGame chess = this.getColor();
 
         ChessPieceColor spieler2Color = chess.pick(SPIELER1, ChessPieceColor.white);
@@ -67,7 +67,7 @@ public class UsageTests {
     }
 
     @Test(expected = GameException.class)
-    public void failurePickSymbol3times() throws GameException, StatusException {
+    public void failurePickSymbol3times() throws GameException, StatusException { //3 players want to log in
         ChessGame chess = this.getColor();
         chess.pick(SPIELER1, ChessPieceColor.white);
         chess.pick(SPIELER2, ChessPieceColor.white);
@@ -75,7 +75,7 @@ public class UsageTests {
     }
 
     @Test
-    public void goodPickSymbol5() throws GameException, StatusException {
+    public void goodPickSymbol5() throws GameException, StatusException { //player wants another color
         ChessGame chess = this.getColor();
         ChessPieceColor spieler1Color = chess.pick(SPIELER1, ChessPieceColor.white);
 
@@ -87,7 +87,7 @@ public class UsageTests {
     }
 
   @Test
-    public void goodSet1() throws GameException, StatusException {
+    public void goodSet1() throws GameException, StatusException { //player sets piece
         ChessGame chess = this.getPosition();
         ChessPieceColor spieler1Color = chess.pick(SPIELER1, ChessPieceColor.white);
         ChessPieceColor spieler2Color = chess.pick(SPIELER2, ChessPieceColor.black);
@@ -98,18 +98,18 @@ public class UsageTests {
   }
 
     @Test(expected = GameException.class)
-    public void failureSetOutside() throws GameException, StatusException {
+    public void failureSetOutside() throws GameException, StatusException { //set piece outside the board
         ChessGame chess = this.getPosition();
         ChessPieceColor spieler1Color = chess.pick(SPIELER1, ChessPieceColor.white);
         ChessPieceColor spieler2Color = chess.pick(SPIELER2, ChessPieceColor.black);
 
-        ChessBoardPosition position = new ChessBoardPosition("X", 100);
+        ChessBoardPosition position = new ChessBoardPosition("X", 1);
 
         chess.set(ChessPieceColor.white, position);
     }
 
     @Test(expected = GameException.class)
-    public void failureSetOutside2() throws GameException, StatusException {
+    public void failureSetOutside2() throws GameException, StatusException { //the piece outside board
         ChessGame chess = this.getPosition();
         ChessPieceColor spieler1Color = chess.pick(SPIELER1, ChessPieceColor.white);
         ChessPieceColor spieler2Color = chess.pick(SPIELER2, ChessPieceColor.black);
@@ -188,13 +188,107 @@ public class UsageTests {
 
         chess.set(ChessPieceColor.black, position);
         chess.set(ChessPieceColor.black, position);
-
     }
 
 
+    //Test boundaries: 1-8 and a-h
+    @Test (expected = Exception.class)
+    public void boardRand() throws Exception {
+        ChessBoardPosition position = new ChessBoardPosition("i",1);
+    }
 
+    @Test (expected = Exception.class)
+    public void boardRand1() throws Exception {
+        ChessBoardPosition position = new ChessBoardPosition("1",1);
+    }
 
+    @Test (expected = Exception.class)
+    public void boardRand2() throws Exception {
+        ChessBoardPosition position = new ChessBoardPosition("-1",1);
+    }
 
+    @Test (expected = Exception.class)
+    public void boardRand3() throws Exception {
+        ChessBoardPosition position = new ChessBoardPosition("-1",9);
+    }
 
+    @Test
+    public void GoodBoardRand()  {
+        ChessBoardPosition position = new ChessBoardPosition("a",1);
+    }
+
+    @Test
+    public void GoodBoardRand1()  {
+        ChessBoardPosition position = new ChessBoardPosition("h",8);
+    }
+
+    //check total chess pieces
+    @Test
+    public void goodPieceNumberStart() {// 32 chess pieces total in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setTotalPieces(32);
+    }
+
+    public void goodPieceNumberTotal() { //total pieces are all black and white pieces together
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.totalPieces = total.blackPieces + total.whitePieces;
+
+    }
+
+    @Test (expected = Exception.class)
+    public void badPieceNumberStart() {//no more than 32 pieces in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setTotalPieces(40);
+    }
+
+    @Test (expected = Exception.class)
+    public void badPieceNumberStart1() {//no less than 32 pieces in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setTotalPieces(31);
+    }
+
+    @Test (expected = Exception.class)
+    public void badPieceNumberStart2() {//no empty board in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setTotalPieces(31);
+    }
+
+    //check black pieces
+    @Test
+    public void goodBlackNumberStart() {// 16 chess pieces total in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setBlackPieces(16);
+    }
+
+    @Test (expected = Exception.class)
+    public void badBlackNumberStart() {//no more than 16 pieces in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setBlackPieces(17);
+    }
+
+    @Test (expected = Exception.class)
+    public void badBlackNumberStart1() {//no less than 16 pieces in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setBlackPieces(15);
+    }
+
+    //check white pieces
+    @Test
+    public void goodWhiteNumberStart() {// 16 chess pieces total in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setWhitePieces(16);
+    }
+
+    @Test (expected = Exception.class)
+    public void badWhiteNumberStart() {//no more than 16 pieces in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setWhitePieces(17);
+    }
+
+    @Test (expected = Exception.class)
+    public void badWhiteNumberStart1() {//no less than 16 pieces in the beginning
+        ChessPieceNumber total = new ChessPieceNumber();
+        total.setWhitePieces(15);
+    }
 
 }
